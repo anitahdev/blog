8b46df339d1c51fce9eac16bee2d2ea7bac23351
anitahdev <anitahoroszko@gmail.com>
2022-10-19 01:35:52 +0200

    home button

--- src/components/ButtonAppBar.tsx
+++ src/components/ButtonAppBar.tsx
@@ -2,25 +2,24 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 
 function appBarLabel() {
   return (
     <Toolbar>
-      <IconButton
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ marginLeft: 3 }}
-      >
-        <Link to={"/"} style={{ textDecoration: "none", color: "white" }}>
-          <Typography variant="h6" component="div">
-            Blog
-          </Typography>
-        </Link>
-      </IconButton>
+      <Typography variant="h6" component="div">
+        Blog
+      </Typography>
+      <Link to={"/"} style={{ textDecoration: "none", color: "white" }}>
+        <HomeIcon
+          color="inherit"
+          aria-label="menu"
+          sx={{ marginLeft: 3 }}
+          fontSize="large"
+        />
+      </Link>
     </Toolbar>
   );
 }
